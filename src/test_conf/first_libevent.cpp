6aed017e817f0a2b862d47a1333924e57a961e0d
lixujun <lxj@linux.com>
2022-07-29 16:18:30 +0800

    摘要：libevent学习代码提交

---
+++ src/test_conf/first_libevent.cpp
@@ -0,0 +1,72 @@
+﻿#include <event2/event.h>
+#include <iostream>
+using namespace std;
+
+void printSupprotedMethods(event_base *base)
+{
+	/*
+	const char **methods = event_get_supported_methods();
+	cout << "Current System Supported Methods******" << endl;
+	for (; *methods != NULL; methods++) {
+		cout << *methods << endl;
+	}
+	*/
+
+	const char *currentMethods = event_base_get_method(base);
+	cout << "Current Methods is " << currentMethods << endl;
+}
+
+void printSupportedFeatures(event_base *base)
+{
+	int features = event_base_get_features(base);
+
+	cout << "Supported Features******" << endl;
+	if (features & EV_FEATURE_ET) {
+		cout << "EV_FEATURE_ET" << endl;
+	} 
+
+	if (features & EV_FEATURE_EARLY_CLOSE) {
+		cout << "EV_FEATURE_EARLY_CLOSE" << endl;
+	}
+
+	if (features & EV_FEATURE_FDS) {
+		cout << "EV_FEATURE_FDS" << endl;
+	}
+
+	if (features & EV_FEATURE_O1) {
+		cout << "EV_FEATURE_O1" << endl;
+	}
+}
+
+int main()
+{
+#ifdef _WIN32 
+	//初始化socket库
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2,2),&wsa);
+#endif
+	// 创建配置上下文
+	event_config *conf = event_config_new();
+
+	// 如果设置了EV_FEATURE_FDS，其他特征就无法设置, 此特征不支持epoll
+	// event_config_require_features(conf, EV_FEATURE_FDS);
+	// event_config_require_features(conf, EV_FEATURE_ET | EV_FEATURE_EARLY_CLOSE | EV_FEATURE_O1);
+	
+	// 设置网络模型使用select
+	// event_config_avoid_method(conf, "epoll");
+	// event_config_avoid_method(conf, "poll");
+	
+	// 根据配置项 conf 初始化libevent上下文
+	event_base *base = event_base_new_with_config(conf);
+	event_config_free(conf);
+	if (NULL == base) {
+		cout << "NULL point: base, event_base_new_with_config failed!" << endl;
+		exit(-1);
+	}
+
+	printSupprotedMethods(base);
+	printSupportedFeatures(base);
+	event_base_free(base);
+
+	return 0;
+}
--- src/test_server/test_server.cpp
+++ src/test_server/test_server.cpp
@@ -1,31 +1,17 @@
-﻿/*/*******************************************************************************
-**                                                                            **
-**                     Jiedi(China nanjing)Ltd.                               **
-**	               创建：丁宋涛 夏曹俊，此代码可用作为学习参考                **
-*******************************************************************************/
-
-/*****************************FILE INFOMATION***********************************
-**
-** Project       : Libevent C＋＋高并发网络编程
-** Contact       : xiacaojun@qq.com
-**  博客   : http://blog.csdn.net/jiedichina
-**	视频课程 : 网易云课堂	http://study.163.com/u/xiacaojun		
-			   腾讯课堂		https://jiedi.ke.qq.com/				
-			   csdn学院		http://edu.csdn.net/lecturer/lecturer_detail?lecturer_id=961	
-**             51cto学院	http://edu.51cto.com/lecturer/index/user_id-12016059.html	
-** 			   老夏课堂		http://www.laoxiaketang.com 
-**                 
-**  Libevent C＋＋高并发网络编程 课程群 ：1003847950 加入群下载代码和交流
-**   微信公众号  : jiedi2007
-**		头条号	 : 夏曹俊
-**
-*****************************************************************************
-//！！！！！！！！！ Libevent C＋＋高并发网络编程 课程  QQ群：1003847950 下载代码和交流*/
-#include <event2/event.h>
+﻿#include <event2/event.h>
 #include <event2/listener.h>
 #include <iostream>
 #include <signal.h>
+#include <string.h>
 using namespace std;
+
+#define SPORT 5001
+
+void listen_cb(struct evconnlistener *e, evutil_socket_t s, struct sockaddr *addr, int socklen, void * arg)
+{
+	cout << "listen_cb" << endl;
+}
+
 int main()
 {
 #ifdef _WIN32 
@@ -41,19 +27,38 @@ int main()
 
     std::cout << "test libevent!\n"; 
 	//创建libevent的上下文
-	event_base * base = event_base_new();
-	if (base)
-	{
-		cout << "event_base_new success!" << endl;
+	event_base *base = event_base_new();
+	if (NULL == base) {
+		cout << "NULL point: base" << endl;
+		exit(-1);
 	}
+	
+	cout << "event_base_new success!" << endl;
 
 	// 监听端口
-	// socket, bind, listen
-	evconnlistener_new_bind(base, 
-		
+	// socket, bind, listen 绑定事件
+	struct sockaddr_in sin;
+	memset(&sin, 0, sizeof(struct sockaddr_in));
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(SPORT);
+
+	evconnlistener *ev = evconnlistener_new_bind(base,  // libevent的上下文
+		listen_cb,                 // 接收到连接的上下文
+		base,                      // 回调函数获取的参数
+		LEV_OPT_REUSEABLE | LEV_OPT_CLOSE_ON_FREE, // 地址重用，evconnlistener关闭同时关闭socket
+		10,                        // 连接队列大小（对应listen函数）
+		(sockaddr*)&sin,
+		sizeof(sin)
 	);
-	if (NULL != base) {
-		event_base_free(base);
+
+	if (NULL == ev) {
+		cout << "NULL point: ev" << endl;
+		exit(-1);
 	}
+
+	// 事件分发处理
+	event_base_dispatch(base);
+	evconnlistener_free(ev);
+	event_base_free(base);
 	return 0;
 }
